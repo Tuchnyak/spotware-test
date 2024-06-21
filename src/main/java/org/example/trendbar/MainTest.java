f9045dd157a44559053f2728d2ec2ee8aa5409f8
tuchnyak <georgii.o.sv@gmail.com>
2024-06-22 01:16:07 +0400

    add spring context

--- src/main/java/org/example/trendbar/MainTest.java
+++ src/main/java/org/example/trendbar/MainTest.java
@@ -3,15 +3,18 @@ package org.example.trendbar;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Instant;
+import java.time.ZoneId;
+
 /**
  * @author tuchnyak (George Shchennikov)
  */
 public class MainTest {
 
-    private static Logger logger = LoggerFactory.getLogger(MainTest.class) ;
+    private static final Logger logger = LoggerFactory.getLogger(MainTest.class) ;
 
     public static void main(String[] args) {
-        logger.info("Hello world!");
+        logger.info("Hello world! {}", Instant.now().atZone(ZoneId.systemDefault()));
     }
 
 }
