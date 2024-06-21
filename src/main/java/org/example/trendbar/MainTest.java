0236bae5d6fc990646ccd0a1baae416d474192b4
tuchnyak <georgii.o.sv@gmail.com>
2024-06-22 01:03:11 +0400

    add logging

---
+++ src/main/java/org/example/trendbar/MainTest.java
@@ -0,0 +1,17 @@
+package org.example.trendbar;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author tuchnyak (George Shchennikov)
+ */
+public class MainTest {
+
+    private static Logger logger = LoggerFactory.getLogger(MainTest.class) ;
+
+    public static void main(String[] args) {
+        logger.info("Hello world!");
+    }
+
+}
